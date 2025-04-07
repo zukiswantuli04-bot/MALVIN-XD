d7703709b2c1d462b0800e1d9412d10d80f28d8e
Malvin King <malvintech2@gmail.com>
2025-04-07 16:55:24 +0200

    Add files via upload

---
+++ data/king.js
@@ -0,0 +1,25 @@
+module.exports = {
+  ADMIN_IMG: process.env.ADMIN_IMG || "", // 👤 ᴀᴅᴍɪɴ ᴍᴇɴᴜ
+  PRIVATE_IMG: process.env.PRIVATE_IMG || "", // 📵 ᴘʀɪᴠᴀᴛᴇ ᴍᴇɴᴜ
+  MAIN_IMG: process.env.MAIN_IMG || "", // 👻 ᴍᴇɴᴜ ᴍᴇɴᴜ
+  INFO_IMG: process.env.INFO_IMG || "", // 🔎 ɪɴғᴏ ᴍᴇɴᴜ
+  SETTINGS_IMG: process.env.SETTINGS_IMG || "", // ⚙️ sᴇᴛᴛɪɴɢs ᴍᴇɴᴜ
+  OWNER_IMG: process.env.OWNER_IMG || "", // 👑 ᴏᴡɴᴇʀ ᴍᴇɴᴜ
+  DOWNLOAD_IMG: process.env.DOWNLOAD_IMG || "", // 📥 ᴅᴏᴡɴʟᴏᴀᴅ ᴍᴇɴᴜ
+  MOVIE_IMG: process.env.MOVIE_IMG || "", // 📽 ᴍᴏᴠɪᴇ ᴍᴇɴᴜ
+  MAINMENU_IMG: process.env.MAINMENU_IMG || "", // 📋 ᴍᴀɪɴ ᴍᴇɴᴜ
+  GROUP_IMG: process.env.GROUP_IMG || "", // 👨‍👩‍👦‍👦 ɢʀᴏᴜᴘ ᴍᴇɴᴜ
+  CONVERT_IMG: process.env.CONVERT_IMG || "", // 🔄 ᴄᴏɴᴠᴇᴛ ᴍᴇɴᴜ
+  SEARCH_IMG: process.env.SEARCH_IMG || "", // 🔤 sᴇᴀʀᴄʜ ᴍᴇɴᴜ
+  UTILITY_IMG: process.env.UTILITY_IMG || "", // 🛑 ᴜᴛɪʟɪᴛʏ ᴍᴇɴᴜ
+  FUN_IMG: process.env.FUN_IMG || "", // 😁 ғᴜɴ ᴍᴇɴᴜ
+  TOOLS_IMG: process.env.TOOLS_IMG || "", // 🔗 ᴛᴏᴏʟs ᴍᴇɴᴜ
+  STICKER_IMG: process.env.STICKER_IMG || "", // 🙃 sᴛɪᴄᴋᴇʀ ᴍᴇɴᴜ
+  RANDOM_IMG: process.env.RANDOM_IMG || "", // 🗣️ ʀᴀɴᴅᴏᴍ ᴍᴇɴᴜ
+  MISC_IMG: process.env.MISC_IMG || "", // ⚙️ ᴍɪsᴄ ᴍᴇɴᴜ
+  ANIME_IMG: process.env.ANIME_IMG || "", // 🎌 ᴀɴɪᴍᴇ ᴍᴇɴᴜ
+  SUPPORT_IMG: process.env.SUPPORT_IMG || "", // 💬 sᴜᴘᴘᴏʀᴛ ᴍᴇɴᴜ
+  AI_IMG: process.env.AI_IMG || "", // 🤖 ᴀɪ ᴍᴇɴᴜ
+  OTHER_IMG: process.env.OTHER_IMG || "", // 🕳 ᴏᴛʜᴇʀ ᴍᴇɴᴜ
+  NSFW_IMG: process.env.NSFW_IMG || "", // 🔞 ɴsғᴡ ᴍᴇɴᴜ
+};
