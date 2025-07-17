6820c89f58c5e99f595bb706cac09867f446aa7b
Malvin King <malvintech2@gmail.com>
2025-07-18 00:26:11 +0200

    Update and rename settings.js to settingss.js

---
+++ settingss.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  PREFIX: "." // prefix (e.g., ., /, !, *)
+};
